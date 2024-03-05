74a7fb4fd7aa1ee83232a771dd737cc99bc0d0c2
Toby <therealtobes28@gmail.com>
2024-03-05 09:51:41 +0000

    Add nix config

---
+++ replit.nix
@@ -0,0 +1,24 @@
+{pkgs}: {
+  deps = [
+	pkgs.portmidi
+	pkgs.libpng
+	pkgs.libjpeg
+	pkgs.fontconfig
+	pkgs.SDL2_ttf
+	pkgs.SDL2_mixer
+	pkgs.SDL2_image
+	pkgs.SDL2
+	pkgs.xsimd
+	pkgs.libxcrypt
+	pkgs.tk
+	pkgs.tcl
+	pkgs.qhull
+	pkgs.pkg-config
+	pkgs.gtk3
+	pkgs.gobject-introspection
+	pkgs.ghostscript
+	pkgs.freetype
+	pkgs.ffmpeg-full
+	pkgs.cairo
+  ];
+}
